cec46361a87b03b2442b8abc6aa225feba89d26f
Alex Sobolev <aleksandr.sobolev@reef.pl>
2025-10-07 14:32:34 +0600

    feat: update project dependencies, get rid of unused libraries

--- src/abstract_block_dumper/apps.py
+++ src/abstract_block_dumper/apps.py
@@ -7,4 +7,4 @@ class AbstractBlockDumperConfig(AppConfig):
     verbose_name = "Abstract Block Dumper"
 
     def ready(self) -> None:
-        return super().ready()
+        from abstract_block_dumper.executor import celery_unit
